6f341c1d9d6bdc01c5611875b144a28008653a67
Quatov <nooasarano21@gmail.com>
2026-01-30 11:31:32 +0500

    new solutions for exceptions

---
+++ processing_errors.js
@@ -0,0 +1,45 @@
+﻿/*
+//1
+let first = Number(prompt());
+let second = Number(prompt());
+
+try {
+    console.log(first / second);
+}
+
+catch (error) {
+    console.log(error.message);
+}
+*/
+
+/*
+//2
+let number = prompt();
+
+if (Number.isNaN(Number(number)))
+    console.log('Некорректные данные!');
+else
+    console.log(Number(number) ** 2);
+*/
+
+/*
+//3
+let number = Number(prompt());
+
+function checkHundred(digits) {
+    if (digits + 30 > 100) {
+        throw new Error('Число превышает 100');
+    }
+
+    return digits + 30;
+}
+
+try {
+    console.log(checkHundred(number));
+}
+
+catch (error) {
+    console.log(error.message);
+    console.log(100);
+}
+*/
